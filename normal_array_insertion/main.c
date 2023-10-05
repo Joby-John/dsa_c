76225f35b44a423bb0c9648b59d614538aaa5b6e
Joby-John <joby432john@gmail.com>
2023-10-06 01:25:33 +0530

    modified to avoid overflow

--- normal_array_insertion/main.c
+++ normal_array_insertion/main.c
@@ -13,6 +13,11 @@ void ind_add(int arr[], int* size)
      int index;
     printf("Enter the index you want to insert:- ");
     scanf("%d",&index);
+    if (index>*size)
+    {
+        printf("Index you entered will produce vacant spaces so inserting in %d\n", *size);
+        index = *size;
+    }
     for(int i = *size-1; i>=index; i--)
     {
         arr[i+1] = arr[i];
@@ -26,7 +31,12 @@ int main()
     int size = 5;
     int arr[100] = {1,2,3,4,5};
     display(arr, &size);
+    if (size<100){
     ind_add(arr, &size);
+    }
+    else{
+        printf("Array is full");
+    }
     display(arr,&size);
     return 0;
 }
